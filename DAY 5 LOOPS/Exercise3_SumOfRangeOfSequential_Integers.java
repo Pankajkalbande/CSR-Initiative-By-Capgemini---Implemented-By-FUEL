8403c1aa5d7b2e189440aa131a550495b0e53f89
coderPK <pankajkalbande87@gmail.com>
2022-05-12 06:35:31 +0530

    formated doc

--- DAY 5 LOOPS/Exercise3_SumOfRangeOfSequential_Integers.java
+++ DAY 5 LOOPS/Exercise3_SumOfRangeOfSequential_Integers.java
@@ -10,29 +10,28 @@ Do this by noticing that the sum from low to high is the same as the sum from on
 the sum from one to (low-1). Calculate each of these by using the formula: 
 sum = (n*(n+1))/2 */
 
-
 import java.util.*;
 
 public class Exercise3_SumOfRangeOfSequential_Integers {
     public static void main(String[] args) {
-        
-        Scanner sc=new Scanner(System.in);
-System.out.println("Enter the value of low number:");
-int l=sc.nextInt();
-System.out.println("Enter the value of high number:");
-int h=sc.nextInt();
-int i;
-int sum=0;
-int fsum=0;
-int n=l-1;
-for(i=l;i<=h;i=i+1)
-sum=sum+i;
-fsum = (n*(n+1))/2;
-{
 
-System.out.println(sum);
-System.out.println(fsum);
-}
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of low number:");
+        int l = sc.nextInt();
+        System.out.println("Enter the value of high number:");
+        int h = sc.nextInt();
+        int i;
+        int sum = 0;
+        int fsum = 0;
+        int n = l - 1;
+        for (i = l; i <= h; i = i + 1)
+            sum = sum + i;
+        fsum = (n * (n + 1)) / 2;
+        {
+
+            System.out.println(sum);
+            System.out.println(fsum);
+        }
     }
-    
+
 }
