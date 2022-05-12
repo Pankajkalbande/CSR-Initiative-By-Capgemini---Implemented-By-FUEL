1441512a45050760a50c2e30b93907d15c05cd59
coderPK <pankajkalbande87@gmail.com>
2022-05-12 06:38:52 +0530

    formated doc

--- DAY 5 LOOPS/Exercise7_PrintAlphabets.java
+++ DAY 5 LOOPS/Exercise7_PrintAlphabets.java
@@ -2,13 +2,12 @@
 
 import java.util.*;
 
-class Exercise7_PrintAlphabets{
-public static void main(String[] args){
+class Exercise7_PrintAlphabets {
+    public static void main(String[] args) {
 
-for(char c='a';c<='z';c++)
+        for (char c = 'a'; c <= 'z'; c++)
 
+            System.out.println(c);
 
-System.out.println(c);
-
-}
+    }
 }
