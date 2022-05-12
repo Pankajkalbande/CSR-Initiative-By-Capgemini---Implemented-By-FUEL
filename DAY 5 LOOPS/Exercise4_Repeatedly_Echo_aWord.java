57cbda468bd2cedacaba90b03a8381f140c86dde
coderPK <pankajkalbande87@gmail.com>
2022-05-12 06:37:15 +0530

    formateed doc

--- DAY 5 LOOPS/Exercise4_Repeatedly_Echo_aWord.java
+++ DAY 5 LOOPS/Exercise4_Repeatedly_Echo_aWord.java
@@ -1,3 +1,4 @@
+
 /* Exercise 4 — Repeatedly Echo a Word
 Write a program that asks the user to enter a word. The program will then repeat word for as 
 many times as it has characters: 
@@ -15,24 +16,23 @@ String inputString;
 int times;
 . . . .
 times = inputString.length()*/
- import java.util.*;
+import java.util.*;
 
 public class Exercise4_Repeatedly_Echo_aWord {
     public static void main(String[] args) {
-        
-        Scanner sc=new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a word to echo:");
-        String a=sc.nextLine();
+        String a = sc.nextLine();
         int times;
-        int i=0;
-        times=a.length();
-        while(i<times)
-        
-        
+        int i = 0;
+        times = a.length();
+        while (i < times)
+
         {
-        i=i+1;
-        System.out.println(a);
+            i = i + 1;
+            System.out.println(a);
         }
     }
-    
+
 }
